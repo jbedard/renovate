4b26db3c61f98c928e2741ab7dbce37c131501db
Jason Bedard <jason+github@jbedard.ca>
2022-09-19 15:02:21 -0700

    test: add bazel http_archive duplication test

--- lib/modules/manager/bazel/update.spec.ts
+++ lib/modules/manager/bazel/update.spec.ts
@@ -456,4 +456,61 @@ describe('modules/manager/bazel/update', () => {
       expect(res).toEqual(output);
     });
   });
+
+  it('updates one http_archive alongside others', async () => {
+    const inputHash1 =
+      '5aef09ed3279aa01d5c928e3beb248f9ad32dde6aafe6373a8c994c3ce643064';
+
+    const input = `
+        http_archive(
+            name = "aspect_rules_js",
+            sha256 = "db9f446752fe4100320cf8487e8fd476b9af0adf6b99b601bcfd70b289bb0598",
+            strip_prefix = "rules_js-1.1.2",
+            url = "https://github.com/aspect-build/rules_js/archive/refs/tags/v1.1.2.tar.gz",
+        )
+
+        http_archive(
+            name = "rules_nodejs",
+            sha256 = "${inputHash1}",
+            urls = ["https://github.com/bazelbuild/rules_nodejs/releases/download/5.5.3/rules_nodejs-core-5.5.3.tar.gz"],
+        )
+      `.trim();
+
+    const currentValue1 = '5.5.3';
+    const newValue1 = '5.5.4';
+    const upgrade1 = {
+      depName: 'rules_nodejs',
+      depType: 'http_archive',
+      repo: 'bazelbuild/rules_nodejs',
+      managerData: { def: input },
+      currentValue: currentValue1,
+      newValue: newValue1,
+    };
+
+    const tarContent1 = Buffer.from('foo');
+    const outputHash1 = crypto
+      .createHash('sha256')
+      .update(tarContent1)
+      .digest('hex');
+
+    httpMock
+      .scope('https://github.com')
+      .get(
+        '/bazelbuild/rules_nodejs/releases/download/5.5.4/rules_nodejs-core-5.5.4.tar.gz'
+      )
+      .reply(200, tarContent1);
+
+    const output1 = input
+      .replace(currentValue1, newValue1)
+      .replace(currentValue1, newValue1)
+      .replace(currentValue1, newValue1)
+      .replace(inputHash1, outputHash1);
+
+    // FAIL: duplicates the first http_archive ("aspect_rules_js")
+    const res = await updateDependency({
+      fileContent: input,
+      upgrade: upgrade1,
+    });
+    expect(res).toEqual(output1);
+  });
 });
